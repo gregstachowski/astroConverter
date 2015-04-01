f8727c61a99b9a1e44f8737382b302b7cd4e6932
Laszlowaty <laszla.niet@gmail.com>
2015-04-01 13:17:50 +0100

    added converting tests and more materials in mats/*.txt

---
+++ tests.py
@@ -0,0 +1,31 @@
+from algorithms.converts import asas, integral, nsvs, hipparcos
+
+
+def test_asas_converting():
+    file = open("mats/asas.txt").read()
+    print(asas(file))
+
+
+def test_integral_converting():
+    file = open("mats/integral.txt").read()
+    print(integral(file))
+
+
+def test_nsvs_converting():
+    file = open("mats/nsvs.txt").read()
+    print(nsvs(file))
+
+
+def test_hipparcos_converting():
+    file = open("mats/hipparcos.txt").read()
+    print(hipparcos(file))
+
+
+def converting_tests():
+    test_asas_converting()
+    test_nsvs_converting()
+    test_integral_converting()
+    test_hipparcos_converting()
+
+if __name__ == "__main__":
+    pass
