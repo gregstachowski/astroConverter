822d0497832f7ffc95e9648a1ced9b5019a9d969
laszlowaty <laszla.niet@gmail.com>
2015-04-24 11:47:03 +0200

    rewritten plotting
    New, efficient system for ploting. Selecting, deleting and clearing
    selection are now working properly and efficient as well. closes #8

--- algorithms/plots.py
+++ algorithms/plots.py
@@ -1,75 +1,49 @@
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Button, RectangleSelector
 from time import time, sleep
+from algorithms.tests import perftest
 # TODO:
-# point finder
-# change color of chosen point
-# delete points
-# clear point selection (in case of errors)
-# update text_field (COULD BE TRICKY!)
+# point finder \/
+# change color of chosen point \/
+# delete points \/
+# clear point selection (in case of errors) \/
+# update text_field (COULD BE TRICKY!) 
+# make use of Points.base_config. Best if everything will be in settings.py
 
-class Point(object):
-    
-    base_config={'color': 'b',
-                 'select_color': 'r'}
-
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        self.color = Point.base_config['color']
-        
-    def __str__(self):
-        return str(self.x) + " - " + str(self.y) + "Color: " + self.color
-    
-    @staticmethod
-    def delete_points(event):
-        todelete = []
-        for point in Plot.plot.points:
-            if point.color == 'r':
-                todelete.append(point)
-        for td in todelete:
-            Plot.plot.points.remove(td)
-        Plot.plot.redraw()
-    
+class Points:
+    base_config = {}
 
 class Plot(object):
-    """
-    *******************
-    HAS TO BE REWRITTEN
-    *******************
-    """
+    
     plot = None # Remember to update reference. It may be important!
 
     def __init__(self, list, master):
         #print(list)
         self.list = list
         Plot.plot = self
-        self.master = master #it's needed for updating text_field
-        self.points = []
-        self.create_points(list)
-        self.create_graph()
+        self.master = master #it's needed for updating text_field #THINKOVER! MAYBE IT'S NOT NEEDED
+        self.unselected_points = list
+        self.selected_points = [[],[]]
+        self.create_initial_graph()
         
+    def draw_unselected_points(self):
+        plt.axes(self.ax)
+        #print("Unselected: ", end="")
+        #print(self.unselected_points)
+        self.ax.plot(self.unselected_points[0], self.unselected_points[1], 'o', color='b')
+        
+    def draw_selected_points(self):
+        plt.axes(self.ax)
+        #print("Selected: ", end="")
+        #print(self.selected_points)
+        self.ax.plot(self.selected_points[0], self.selected_points[1], 'o', color='r')
     
-    def create_points(self, list):
-        for x in range(len(list[0])):
-            x_cord = list[0][x-1]
-            y_cord = list[1][x-1]
-            self.points.append(Point(x_cord, y_cord))
-            
-    def get_list_of_cords(self):
-        cordsx = []
-        cordsy = []
-        for point in self.points:
-            cordsx.append(point.x)
-            cordsy.append(point.y)
-        return (cordsx, cordsy)
-            
-    def create_graph(self):
+    def create_initial_graph(self):
         self.ax = plt.subplot(111)
-        #for p in self.points:
-        #    plt.plot(p.x, p.y, 'o', color=p.color)
-        cords = self.get_list_of_cords()
-        plt.plot(cords[0], cords[1], 'o', color='b')
+        #for line in self.ax.axes.get_lines():
+        #    print(dir(line))
+        #    print(line.get_xdata())
+        self.draw_unselected_points()
         plt.subplots_adjust(bottom=0.2)
         axdel = plt.axes([0.7, 0.05, 0.1, 0.075])
         axselect = plt.axes([0.81, 0.05, 0.1, 0.075])
@@ -79,49 +53,67 @@ class Plot(object):
         bselect = Button(axselect, 'Select')
         bselect.on_clicked(Select_handler)
         bdel = Button(axdel, 'Delete')
-        bdel.on_clicked(Point.delete_points)
+        bdel.on_clicked(Select_handler.del_selected)
         plt.show()
-        
+
+    @perftest
     def redraw(self):
         plt.axes(self.ax)
         plt.cla()
-        for p in self.points:
-            self.ax.plot(p.x, p.y, 'o', color=p.color)
+        #for p in self.points:
+        #    plt.plot(p.x, p.y, 'o', color=p.color)
+        #unselected_points = self.get_unselected_points()
+        self.draw_selected_points()
+        self.draw_unselected_points()
         plt.draw()
-        
+
         
 class Select_handler(RectangleSelector):
     
     def __init__(self, event):
         RectangleSelector.__init__(self, Plot.plot.ax, onselect=self.onselect)
-        
+     
+    @perftest   
     def onselect(self, eclick, erelease):
-        def is_in_range(point):
-            x = float(point.x)
-            y = float(point.y)
+        #print("testujemy: ", end="")
+        def is_in_range(xdata, ydata):
+            x = float(xdata)
+            y = float(ydata)
             if eclick.xdata <= x <= erelease.xdata or erelease.xdata <= x <= eclick.xdata:
                 if eclick.ydata <= y <= erelease.ydata or erelease.ydata <= y <= eclick.ydata:
                     return True
-        print("Eclick: %s - %s" %(eclick.xdata, eclick.ydata))
-        print("Erelease: %s - %s" % (erelease.xdata, erelease.ydata))
-        t1 = time()
-        for point in Plot.plot.points:
-            if is_in_range(point):
-               point.color = 'r'
-        t2 = time()
+        unsel = Plot.plot.unselected_points
+        sel = Plot.plot.selected_points
+        all_unsel_len = len(unsel[0])
+        to_del = []
+        #print (all_unsel_len)
+        for iter in range(all_unsel_len):
+            xcord = unsel[0][iter]
+            ycord = unsel[1][iter]
+            if is_in_range(xcord, ycord):
+                sel[0].append(xcord)
+                sel[1].append(ycord)
+                to_del.append(iter)
+        to_del.sort(reverse=True)
+        for index in to_del:
+            del unsel[0][index]
+            del unsel[1][index]
+        self.disconnect_events()
         Plot.plot.redraw()
-        t3 = time()
-        print("Points: %s\nLoop: %s\nRedraw: %s" %(len(Plot.plot.points), t2-t1, t3-t2))
-        self.set_active(False)
-        
+                
     @staticmethod
     def clear_selection(event):
         """event is unused"""    
-        for point in Plot.plot.points:
-            point.color = 'b'
+        for iter in range(len(Plot.plot.selected_points[0])):
+            Plot.plot.unselected_points[0].append(Plot.plot.selected_points[0][iter])
+            Plot.plot.unselected_points[1].append(Plot.plot.selected_points[1][iter])
+        Plot.plot.selected_points = [[],[]]
         Plot.plot.redraw()
         
-        
+    @staticmethod
+    def del_selected(event):
+        Plot.plot.selected_points = [[],[]]
+        Plot.plot.redraw()
         
         
         
