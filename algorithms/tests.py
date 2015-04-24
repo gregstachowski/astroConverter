ba49b90a4e9ca4cb850d266f106fb56ff24e3da9
laszlowaty <laszla.niet@gmail.com>
2015-04-24 11:43:14 +0200

    adding file for tests It contains only perforamce test. More will be added

---
+++ algorithms/tests.py
@@ -0,0 +1,28 @@
+from time import sleep, time
+
+
+def start():
+    return time() * 1000
+
+
+def end(t):
+    return time()*1000 - t
+
+
+#@perftest
+def perftest(f):
+    def new_f(*args, **kws):
+        _t = start()
+        returns = f(*args, **kws)
+        _t = end(_t)
+        print("%s took %s ms" % (f.__name__, _t)) 
+        return returns
+    return new_f
+
+@perftest
+def bum():
+    sleep(1)
+
+    
+if __name__ == '__main__':
+    pass
