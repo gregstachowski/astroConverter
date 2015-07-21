4edb6d0405aa23fd76004e1b7ed02fb2a4b74b37
Laszlo <laszla.niet@gmail.com>
2015-07-22 00:15:00 +0200

    PEP8

--- settings.py
+++ settings.py
@@ -13,12 +13,12 @@ The above copyright notice and this permission notice shall be included in all c
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 """
 
+
 class Points:
-    base_config = {
-                   'unselected_point_color': 'b',
-                   'selected_point_color' : 'r',
+    base_config = {'unselected_point_color': 'b',
+                   'selected_point_color': 'r',
                    'unselected_point_line_type': '.',
-                   'selected_point_line_type' : '.',
+                   'selected_point_line_type': '.',
                    'x_label': 'x_label',
                    'y_label': 'y_label',
                    }
